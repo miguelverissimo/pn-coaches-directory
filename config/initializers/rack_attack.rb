ffb0660b2de9372638e3ba16e91e2eb6aa067480
Miguel Verissimo <m.verissimo@gmail.com>
2020-02-06 11:34:24 -0500

    configure rack-attack
    Add a simple rule to block too many requests from a single ip

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,6 @@
+# Throttle all requests by IP (60rpm)
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+    req.ip
+  end
