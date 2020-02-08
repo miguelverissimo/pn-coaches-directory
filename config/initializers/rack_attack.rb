dd350bda82555c5d5a4c8820a7a93aaf5b7f64a1
Miguel Verissimo <m.verissimo@gmail.com>
2020-02-07 20:49:00 -0500

    Fix Rack::Attack initializer

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,7 @@
-# Throttle all requests by IP (60rpm)
-  #
+class Rack::Attack
+  # Throttle all requests by IP (60rpm)
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
   throttle('req/ip', limit: 300, period: 5.minutes) do |req|
     req.ip
   end
+end
