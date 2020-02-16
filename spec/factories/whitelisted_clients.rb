7d4705c53ae1af257cce28db3e0231b942c4de03
Miguel Verissimo <m.verissimo@gmail.com>
2020-02-16 11:38:12 -0500

    wip

---
+++ spec/factories/whitelisted_clients.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :whitelisted_client do
+    name { "MyString" }
+    domain { "MyString" }
+    token { "MyString" }
+    granted_on { "2020-02-15 19:38:29" }
+    granted_by { "MyString" }
+    last_accessed_on { "2020-02-15 19:38:29" }
+    last_accessed_by { "MyString" }
+    notes { "MyString" }
+  end
+end
